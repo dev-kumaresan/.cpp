18915cbe8ced705c81e33329dc4418d5c56c37be
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-29 21:51:20 +0530

    Program Updated ;

--- classes-and-objects/class-object.cpp
+++ classes-and-objects/class-object.cpp
@@ -1,1 +1,17 @@
+// structure of CPP | Structure Class
 
+#include<iostream>
+using namespace std;
+class A                      //A is the class name
+{
+   public:
+   void fun()
+   {                //function definition
+    cout<<"Class Executed";
+   }
+   
+};
+int main(){
+    A a;            // a is the object
+    a.fun();        // call the function in inside of the class help of object
+}
